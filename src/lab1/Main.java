e3f1c284384d22871ea1d9e805c631b631055a41
Alexander Demidenko <omgflyxd@icloud.com>
2024-06-27 20:08:14 +0300

    lab

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,7 +1,54 @@
 package lab1;
+import java.util.Scanner;
 
-class HelloWorld {
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Я");
+        System.out.println("хорошо");
+        System.out.println("знаю");
+        System.out.println("Java.");
+
+        int x = (46 + 10) * (10 / 3);
+        System.out.println(x);
+        int y = (29) * (4) * (-15);
+        System.out.println(y);
+
+        int number = 10500;
+        int result = (number / 10) / 10;
+        System.out.println(result);
+
+        double a = 3.6;
+        double n = 4.1;
+        double c = 5.9;
+        double result1 = a * n * c;
+        System.out.println(result1);
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextInt()) {
+            int num = scanner.nextInt();
+            System.out.println(num);
+        }
+        scanner.close();
+
+        Scanner scanner1 = new Scanner(System.in);
+        int b = scanner1.nextInt();
+
+        if (b % 2 != 0) {
+            System.out.println("Нечетное");
+        } else if (b > 100) {
+            System.out.println("Выход за пределы диапазона");
+        } else {
+            System.out.println("Четное");
+        }
+
+        scanner.close();
     }
+
+
 }
+
+
+
